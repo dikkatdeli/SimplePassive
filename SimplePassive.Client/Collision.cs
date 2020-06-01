eece033c87f09826cd9226c446e3e1cf0256ed43
Hannele Ruiz <justlemoncl@gmail.com>
2020-06-01 01:17:09 -0400

    Added class to store the collision information of a player

---
+++ SimplePassive.Client/Collision.cs
@@ -0,0 +1,33 @@
+﻿using CitizenFX.Core;
+
+namespace SimplePassive.Client
+{
+    public class Collision
+    {
+        #region Properties
+
+        /// <summary>
+        /// The player that owns this collision information.
+        /// </summary>
+        public Player Owner { get; }
+        /// <summary>
+        /// The vehicle known used by this player.
+        /// </summary>
+        public Vehicle Vehicle { get; private set; } = null;
+        /// <summary>
+        /// The last trailer or towed vehicle used by this pllayer.
+        /// </summary>
+        public Vehicle Hooked { get; private set; } = null;
+
+        #endregion
+
+        #region Constructor
+
+        public Collision(Player player)
+        {
+            Owner = player;
+        }
+
+        #endregion
+    }
+}
